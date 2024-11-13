63552f5a1731237af9eaa031bff35ebb0b69eea6
Beat-Christ <gandachristiannanga@gmail.com>
2024-11-13 14:07:22 +0100

    open + close done

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -12,10 +12,12 @@ async function getWorks(filter) {
             const filtered = json.filter((data) => data.categoryId === filter);      
             for(let i = 0; i < filtered.length; i++) {
                 setFigure(filtered[i]);
+                setModalFigure(filtered[i]);
             }
         }else{
             for(let i = 0; i < json.length; i++) {
                 setFigure(json[i]);
+                setModalFigure(json[i]);
             }
         }
         
@@ -33,6 +35,15 @@ function setFigure(data) {
 				<figcaption>${data.title}</figcaption>`;
 
     document.querySelector(".gallery").append(figure);
+
+}
+
+function setModalFigure(data) {
+    const figure = document.createElement("figure");
+    figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+				<figcaption>${data.title}</figcaption>`;
+
+    document.querySelector(".gallery-modal").append(figure);
 
 }
 
@@ -75,14 +86,85 @@ document.querySelector(".tous").addEventListener("click", () => getWorks());
 
 function displayAdminMode(){
     if (sessionStorage.authToken) {
-        console.log("ok");
         const editBanner = document.createElement("div");
         editBanner.className = "edit";
         editBanner.innerHTML =
-          '<p><i class="fa-regular fa-pen-to-square"></i>Mode édition</p>';
+          '<p><a href="#modal1" class="js-modal"><i class="fa-regular fa-pen-to-square"></i>Mode édition</a></p>';
         document.body.prepend(editBanner);
+
+        const login = document.querySelector(".login")
+        login.textContent = "logout";
     }
 }
 
 displayAdminMode();
+
+let modal = null;
+const focusableSelector = "button, a, input, textarea";
+let focusables = [];
+
+const openModal = function (e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    focusables = Array.from(modal.querySelectorAll(focusableSelector));
+    focusables[0].focus();
+    modal.style.display = null;
+    modal.removeAttribute("aria-hidden");
+    modal.setAttribute("aria-modal", "true");
+    modal.addEventListener("click", closeModal);
+    modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    modal
+        .querySelector(".js-modal-stop")
+        .addEventListener("click", stopPropagation);
+};
+
+const closeModal = function (e) {
+    if (modal === null) return;
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click", closeModal);
+    modal
+        .querySelector(".js-modal-close")
+        .removeEventListener("click", closeModal);
+    modal
+        .querySelector(".js-modal-stop")
+        .removeEventListener("click", stopPropagation);
+    modal = null;
+};
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+};
+
+const focusInModal = function (e) {
+    e.preventDefault();
+    let index = focusables.findIndex((f) => f === modal.querySelector(":focus"));
+    if (e.shiftkey === true) {
+        index--;
+    } else {
+        index++;
+    }
+    if (index >= focusables.length) {
+        index = 0;
+    }
+    if (index < 0) {
+        index = focusables.length -1;
+    }
+    focusables[index].focus();
+};
+
+window.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e);
+    }
+    if (e.key === "Tab" && modal !== null) {
+        focusInModal(e);
+    }
+});
+
+document.querySelectorAll(".js-modal").forEach((a) => {
+    a.addEventListener("click", openModal);
+});
 
